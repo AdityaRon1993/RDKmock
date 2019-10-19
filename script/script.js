9cb38c242a2fc6e1842dac10c2aa57507a545379
AdityaRon1993 <adityaron1993@gmail.com>
2019-10-19 22:35:14 +0530

    NEW IDEA

--- script/script.js
+++ script/script.js
@@ -3,18 +3,3 @@ $('.nav-link').click((event)=>{
     $(event.target).addClass('active')
 })
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    console.log(' document.documentElement.scrollTop -->', document.documentElement.scrollTop)
-  if (document.documentElement.scrollTop > 150 ) {
-    $(".nav").addClass('scrollActiveNav')
-    $(".name").addClass('scrollActiveName')
-    
-
-  } else if(document.documentElement.scrollTop <= 50 ) {
-    $(".nav").removeClass('scrollActiveNav')
-    $(".name").removeClass('scrollActiveName')
-
-  }
-}
